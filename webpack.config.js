920da5c505e41d57cf10ce95fa6a3d28bc193655
Duouii <14638064+doraemon-doraemon@user.noreply.gitee.com>
2025-11-06 01:00:06 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+// webpack.config.js
+const { VueLoaderPlugin } = require('vue-loader')
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
+
+module.exports = {
+  entry: './src/main.ts',
+  mode: 'production',
+  output: {
+    filename: 'js/[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.vue'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: { appendTsSuffixTo: [/\.vue$/] }, // ⬅ 这行非常关键
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader,'style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext][query]' // 输出路径
+        }
+      }
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css'
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+    splitChunks: {
+      chunks: 'all', // 自动拆分第三方库
+    },
+  },
+}
