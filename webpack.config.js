9a8849d2e48d7894d1ddd708c6f45e3c23a485f7
Duouii <14638064+doraemon-doraemon@user.noreply.gitee.com>
2025-11-06 14:02:30 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     filename: 'js/[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/CICD/',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.ts', '.js', '.vue'],
