ff651452cf306d6511ccdeb3a1ce4002198f13c7
Duouii <14638064+doraemon-doraemon@user.noreply.gitee.com>
2025-11-06 11:10:13 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/CICD/',
   plugins: [vue()],
     resolve: {
       alias: {
